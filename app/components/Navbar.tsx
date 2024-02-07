75b8e8d9aade6a487e17cb4c477bf61586bac277
Tamyis Muchtamil <105982996+tamisbmxx4@users.noreply.github.com>
2024-02-07 16:54:32 +0700

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -8,7 +8,7 @@ export default function Navbar() {
         <div className="flex justify-between items-center w-full">
           <Link href="/">
             <h1 className="text-2xl font-medium">
-              mis. <span className="text-teal-500">pdf</span>
+              NEVER<span className="text-teal-500">MIND</span>
             </h1>
           </Link>
 
